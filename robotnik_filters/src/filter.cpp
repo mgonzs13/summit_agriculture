57f93783b094bc759384da9388165326bb47c5fc
Miguel Ángel González Santamarta <mgons@unileon.es>
2025-03-20 17:58:17 +0100

    setting reliable for filtered 3d lidar topic

--- robotnik_filters/src/filter.cpp
+++ robotnik_filters/src/filter.cpp
@@ -1,32 +1,34 @@
 #define BOOST_BIND_NO_PLACEHOLDERS
 
+#include <algorithm>
+#include <iterator>
 #include <memory>
 #include <string>
-#include <iterator>
-#include <algorithm>
 
 #include "robotnik_filters/filter_node.hpp"
 
-#include <pcl/io/pcd_io.h>
-#include <pcl/point_cloud.h>
 #include <pcl/filters/crop_box.h>
 #include <pcl/filters/statistical_outlier_removal.h>
+#include <pcl/io/pcd_io.h>
+#include <pcl/point_cloud.h>
 #include <pcl/point_types.h>
 #include <pcl_conversions/pcl_conversions.h>
 #include <rclcpp/qos.hpp>
 
 #include "sensor_msgs/msg/point_cloud2.hpp"
 #include "std_msgs/msg/header.hpp"
-    
-FilterNode::FilterNode(const rclcpp::NodeOptions& options)
-: Node("FilterNode", options)
-, publisher_{ create_publisher<sensor_msgs::msg::PointCloud2>("~/output", rclcpp::SensorDataQoS{}) }
-, subscription_{ create_subscription<sensor_msgs::msg::PointCloud2>("~/input", rclcpp::QoS(1).reliable(), std::bind(&FilterNode::topic_callback, this, std::placeholders::_1)) }
-{
-}
 
-void FilterNode::topic_callback(const sensor_msgs::msg::PointCloud2::SharedPtr msg)
-{
+FilterNode::FilterNode(const rclcpp::NodeOptions &options)
+    : Node("FilterNode", options),
+      publisher_{create_publisher<sensor_msgs::msg::PointCloud2>(
+          "~/output", rclcpp::QoS(1).reliable())},
+      subscription_{create_subscription<sensor_msgs::msg::PointCloud2>(
+          "~/input", rclcpp::QoS(1).reliable(),
+          std::bind(&FilterNode::topic_callback, this,
+                    std::placeholders::_1))} {}
+
+void FilterNode::topic_callback(
+    const sensor_msgs::msg::PointCloud2::SharedPtr msg) {
   // ROS2 message to PCL message
   pcl::PCLPointCloud2::Ptr cloud_filtered{new pcl::PCLPointCloud2{}};
   pcl_conversions::toPCL(*msg, *cloud_filtered);
@@ -49,9 +51,9 @@ void FilterNode::topic_callback(const sensor_msgs::msg::PointCloud2::SharedPtr m
   sor.setStddevMulThresh(1.0);
   sor.filter(*cloud_filtered);
 
-  // PCL message to ROS2 message 
+  // PCL message to ROS2 message
   sensor_msgs::msg::PointCloud2 cloud_out;
-  pcl_conversions::fromPCL(*cloud_filtered,cloud_out);  
+  pcl_conversions::fromPCL(*cloud_filtered, cloud_out);
 
   cloud_out.header.frame_id = msg->header.frame_id;
   cloud_out.header.stamp = msg->header.stamp;
