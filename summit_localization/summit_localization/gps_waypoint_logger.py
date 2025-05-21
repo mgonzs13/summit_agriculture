1700b89a16faed99e624f593e5ce467970537ace
Luis Prieto López <55539850+luispri2001@users.noreply.github.com>
2025-05-21 10:13:21 +0200

    Updated gps waypoint follower to use heading from the compass (#4)
    * Updated gps waypoint follower to use heading from the compass
    
    * Removed commet
    
    * Fixed spacing

--- summit_localization/summit_localization/gps_waypoint_logger.py
+++ summit_localization/summit_localization/gps_waypoint_logger.py
@@ -8,9 +8,8 @@ from tkinter import messagebox
 
 import rclpy
 from rclpy.node import Node
-from sensor_msgs.msg import NavSatFix, Imu
-from summit_localization.utils.gps_utils import euler_from_quaternion
-
+from sensor_msgs.msg import NavSatFix
+from std_msgs.msg import Float32
 
 class GpsGuiLogger(tk.Tk, Node):
     """
@@ -26,7 +25,7 @@ class GpsGuiLogger(tk.Tk, Node):
 
         self.gps_pose_label = tk.Label(self, text="Current Coordinates:")
         self.gps_pose_label.pack()
-        self.gps_pose_textbox = tk.Label(self, text="", width=45)
+        self.gps_pose_textbox = tk.Label(self, text="", width=60)
         self.gps_pose_textbox.pack()
 
         self.log_gps_wp_button = tk.Button(
@@ -40,7 +39,7 @@ class GpsGuiLogger(tk.Tk, Node):
         self.last_gps_position = NavSatFix()
 
         self.imu_subscription = self.create_subscription(
-            Imu, "/imu", self.imu_callback, 1
+            Float32, "/imu/compass_heading", self.imu_callback, 1
         )
         self.last_heading = 0.0
 
@@ -51,11 +50,11 @@ class GpsGuiLogger(tk.Tk, Node):
         self.last_gps_position = msg
         self.updateTextBox()
 
-    def imu_callback(self, msg: Imu):
+    def imu_callback(self, msg: Float32):
         """
-        Callback to store the last heading
+        Callback to store the last heading from Float32 message
         """
-        _, _, self.last_heading = euler_from_quaternion(msg.orientation)
+        self.last_heading = msg.data
         self.updateTextBox()
 
     def updateTextBox(self):
@@ -63,26 +62,22 @@ class GpsGuiLogger(tk.Tk, Node):
         Function to update the GUI with the last coordinates
         """
         self.gps_pose_textbox.config(
-            text=f"Lat: {self.last_gps_position.latitude:.6f}, Lon: {self.last_gps_position.longitude:.6f}, yaw: {self.last_heading:.2f} rad"
+            text=f"Lat: {self.last_gps_position.latitude:.6f}, Lon: {self.last_gps_position.longitude:.6f}, Yaw: {self.last_heading:.2f} rad"
         )
 
     def log_waypoint(self):
         """
         Function to save a new waypoint to a file
         """
-        # read existing waypoints
         try:
             with open(self.logging_file_path, "r") as yaml_file:
                 existing_data = yaml.safe_load(yaml_file)
-        # in case the file does not exist, create with the new wps
         except FileNotFoundError:
             existing_data = {"waypoints": []}
-        # if other exception, raise the warining
         except Exception as ex:
             messagebox.showerror("Error", f"Error logging position: {str(ex)}")
             return
 
-        # build new waypoint object
         data = {
             "latitude": self.last_gps_position.latitude,
             "longitude": self.last_gps_position.longitude,
@@ -90,7 +85,6 @@ class GpsGuiLogger(tk.Tk, Node):
         }
         existing_data["waypoints"].append(data)
 
-        # write updated waypoints
         try:
             with open(self.logging_file_path, "w") as yaml_file:
                 yaml.dump(existing_data, yaml_file, default_flow_style=False)
@@ -98,25 +92,20 @@ class GpsGuiLogger(tk.Tk, Node):
             messagebox.showerror("Error", f"Error logging position: {str(ex)}")
             return
 
-        messagebox.showinfo("Info", "Waypoint logged succesfully")
+        messagebox.showinfo("Info", "Waypoint logged successfully")
 
 
 def main(args=None):
     rclpy.init(args=args)
 
-    # allow to pass the logging path as an argument
     default_yaml_file_path = os.path.expanduser("~/gps_waypoints.yaml")
-    if len(sys.argv) > 1:
-        yaml_file_path = sys.argv[1]
-    else:
-        yaml_file_path = default_yaml_file_path
+    yaml_file_path = sys.argv[1] if len(sys.argv) > 1 else default_yaml_file_path
 
     gps_gui_logger = GpsGuiLogger(yaml_file_path)
 
     while rclpy.ok():
-        # we spin both the ROS system and the interface
-        rclpy.spin_once(gps_gui_logger, timeout_sec=0.1)  # Run ros2 callbacks
-        gps_gui_logger.update()  # Update the tkinter interface
+        rclpy.spin_once(gps_gui_logger, timeout_sec=0.1)
+        gps_gui_logger.update()
 
     rclpy.shutdown()
 
